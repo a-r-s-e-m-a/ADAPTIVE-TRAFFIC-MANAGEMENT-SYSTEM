3ca0959192311411b12c89ade98c8ab2a9746f5f
suzumi-dev <betelhem.solomon.mek@gmail.com>
2026-05-15 15:18:14 +0300

    Document contributions of team members
    Added contributions from Arsema Yeshidnber and Betelhem Solomon, detailing their implementations in the project.

--- Group_Members & contribution.cpp
+++ Group_Members & contribution.cpp
@@ -1,1 +1,9 @@
+ARSEMA YESHIDNBER - ETS 0199/17
+   CONTRIBUTION: Implemented the main system loop - handles Auto Mode which runs cycles automatically every 5 at a time until all lanes are cleared, 
+                  Manual Mode where the user controls each cycle, and clean database shutdown on exit
+
+BETELHEM SOLOMON - ETS 0286/17
+  CONTRIBUTION: Implemented the Analytics Dashboard which displays full traffic logs, average vehicles per lane, busiest lane ranking, and total emergency events, 
+               all retrieved directly from the SQLite database and presented in a clean readable table format.
+
 
