e12010e0a71235b76de9b91caa426b3f7738a042
Jude-Jude12 <deresebereket367@gmail.com>
2026-05-15 15:59:25 +0300

    Update Group_Members & contribution.cpp

--- Group_Members & contribution.cpp
+++ Group_Members & contribution.cpp
@@ -5,6 +5,7 @@ ARSEMA YESHIDNBER - ETS 0199/17
 BETELHEM SOLOMON - ETS 0286/17
   CONTRIBUTION: Implemented the Analytics Dashboard which displays full traffic logs, average vehicles per lane, busiest lane ranking, and total emergency events, 
                all retrieved directly from the SQLite database and presented in a clean readable table format.
+
 BEREKET DERESE - ETS 0262/17 
   CONTRIBUTION: Lane Processing and Wait Time  Management- manages traffic lane data by updating vehicle counts, tracking wait times, checking empty lanes, 
                 and calculating total vehicles for efficient traffic control.
